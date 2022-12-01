de42fad801c946cf4c61004a5973b6e518c99b41
ardamoin <ardamoin2@gmail.com>
2022-11-30 22:36:48 -0500

    Refactor code

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let myLibrary = [];
+let table = document.querySelector("tbody");
+const submit = document.querySelector(".submit");
+let bookName = document.querySelector("input#book");
+let author = document.querySelector("input#author");
+let pages = document.querySelector("input#pages");
+let status = document.querySelector("select#read");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -17,5 +23,50 @@ function addBookToLibrary(book) {
     }
 
     if (!duplicate) myLibrary.push(book);
-
 }
+
+function updateTable(book) {
+    let newRow = document.createElement("tr");
+    let title = document.createElement("td");
+    let author = document.createElement("td");
+    let pages = document.createElement("td");
+    let read = document.createElement("td");
+    let readButton = document.createElement("button");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    readButton.textContent = book.read;
+    read.appendChild(readButton);
+
+    newRow.appendChild(title);
+    newRow.appendChild(author);
+    newRow.appendChild(pages);
+    newRow.appendChild(read);
+
+    table.appendChild(newRow);
+}
+
+function clearInputs() {
+    bookName.value = '';
+    author.value = '';
+    pages.value = '';
+    read.value = 'Read';
+}
+
+addBookToLibrary(new Book("Harry Potter", "JK Rowling", "420", "Read"));
+addBookToLibrary(new Book("Game of Thrones", "GRRM", "500", "Read"));
+
+for(let book of myLibrary) {
+    updateTable(book);
+}
+
+
+submit.addEventListener("click", () => {
+    if (bookName.value !== '' && author.value !== '' && pages.value !== '') {
+        const newBook = new Book(bookName.value, author.value, pages.value, read.value);
+        addBookToLibrary(newBook);
+        updateTable(newBook);
+        clearInputs();
+    }
+})
